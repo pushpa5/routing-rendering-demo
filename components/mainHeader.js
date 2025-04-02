2ed9ebda4bf965272e866f08fad90a0dede5b4a0
pushpabhat55 <pushpabhat55@gmail.com>
2025-04-02 12:25:15 +0530

    correct html tag added for accessibility

--- components/mainHeader.js
+++ components/mainHeader.js
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 function MainHeader() {
     return (
-        <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/news">News</Link></li>
-        </ul>
+        <header>
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/news">News</Link></li>
+            </ul>
+        </header>
     )
 }
 
