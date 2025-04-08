f968fe2d10413c2ec44dcd14ca44e4075e0d5c54
pushpabhat55 <pushpabhat55@gmail.com>
2025-04-08 12:06:31 +0530

    server side data fetching when we have different backend server

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,53 +1,60 @@
-// import { DUMMY_NEWS } from "@/dummy-news";
-// import NewsList from "@/components/newsList";
-
-// function News() {
-//     return (
-//         <>
-//             <h1>News Page</h1>
-//             <NewsList news={DUMMY_NEWS} />
-//         </>
-//     )
-// }
-
-// export default News;
-
-'use client'
-
 import NewsList from "@/components/newsList";
-import { useEffect, useState } from "react";
 
-function News() {
-    const [news, setNews] = useState([])
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(true)
+async function News() {
+    const response = await fetch('http://localhost:8080/news');
 
-    useEffect(() => {
-        async function getNews() {
-            setLoading(true)
-            const response = await fetch('http://localhost:8080/news')
-            const news = await response.json();
-            if (!response.ok) {
-                setError('Failed to fetch the news')
-                setLoading(false)
-            }
-            console.log('news', news)
-            setNews(news)
-            setLoading(false)
-        }
-        getNews()
-    }, [])
+    if(!response.ok) {
+        throw new Error('failed to fech the news')
+    }
 
-    const errorContent = !loading && error && <p>unable to fetch the data</p>
+    const news = await response.json()
 
-    const newsContent = !loading && news?.length ?  <NewsList news={news} /> : <p>fetching the data</p>
     return (
         <>
             <h1>News Page</h1>
-            {error ? errorContent : newsContent}
+            <NewsList news={news} />
         </>
     )
 }
 
 export default News;
+
+// 'use client'
+
+// import NewsList from "@/components/newsList";
+// import { useEffect, useState } from "react";
+
+// function News() {
+//     const [news, setNews] = useState([])
+//     const [error, setError] = useState('')
+//     const [loading, setLoading] = useState(true)
+
+//     useEffect(() => {
+//         async function getNews() {
+//             setLoading(true)
+//             const response = await fetch('http://localhost:8080/news')
+//             const news = await response.json();
+//             if (!response.ok) {
+//                 setError('Failed to fetch the news')
+//                 setLoading(false)
+//             }
+//             console.log('news', news)
+//             setNews(news)
+//             setLoading(false)
+//         }
+//         getNews()
+//     }, [])
+
+//     const errorContent = !loading && error && <p>unable to fetch the data</p>
+
+//     const newsContent = !loading && news?.length ?  <NewsList news={news} /> : <p>fetching the data</p>
+//     return (
+//         <>
+//             <h1>News Page</h1>
+//             {error ? errorContent : newsContent}
+//         </>
+//     )
+// }
+
+// export default News;
 
