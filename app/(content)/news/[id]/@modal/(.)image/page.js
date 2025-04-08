2c1cdb75c0548afb38ecc821860f77364bb5256c
pushpabhat55 <pushpabhat55@gmail.com>
2025-04-08 11:23:27 +0530

    using group routing concept

---
+++ app/(content)/news/[id]/@modal/(.)image/page.js
@@ -0,0 +1,24 @@
+'use client'
+
+import { DUMMY_NEWS } from "@/dummy-news"
+import { notFound, useRouter } from "next/navigation"
+
+export default function InterceptionImagePage({ params }) {
+    const router = useRouter()
+    const newSlug = params.id
+    const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newSlug)
+
+    if (!newsItem) {
+        notFound()
+    }
+    return (
+        <>
+            <div className="modal-backdrop" onClick={router.back}/>
+            <dialog className="modal" open>
+                <div className="fullscreen-image">
+                    <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                </div>
+            </dialog>
+        </>
+    )
+}
